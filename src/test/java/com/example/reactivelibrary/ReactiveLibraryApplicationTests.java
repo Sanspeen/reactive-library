fbdda2b6dbbf9d3b230d3c753051bfcb1bfe0b5f
Santiago Franco <ktns0930@gmail.com>
2021-10-25 14:26:34 -0500

    Package structure has been intialized

---
+++ src/test/java/com/example/reactivelibrary/ReactiveLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
